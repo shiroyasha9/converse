3d72fb56cf348f09eb6c4364a01565c9726c0da5
Mubin Ansari <mubinansari910@gmail.com>
2022-10-28 03:03:57 +0530

    save username jotai state in localstorage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import '../styles/index.css';
 
+import { useAtomValue } from 'jotai';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+
+import { usernameAtom } from '../stores/user';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const username = useAtomValue(usernameAtom);
+
+  useEffect(() => {
+    if (username !== null) {
+      setIsLoading(false);
+    }
+  }, [username]);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <>
       <Head>
--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,3 +1,27 @@
 import { atom } from 'jotai';
 
-export const usernameAtom = atom<string>('');
+function atomWithLocalStorage<T>(key: string, initialValue: T) {
+  const getInitialValue = () => {
+    if (typeof window === 'undefined') {
+      return null;
+    }
+    const item = localStorage.getItem(key);
+    if (item !== null) {
+      return JSON.parse(item);
+    }
+    return initialValue;
+  };
+  const baseAtom = atom(getInitialValue());
+  const derivedAtom = atom(
+    (get) => get(baseAtom),
+    (get, set, update) => {
+      const nextValue =
+        typeof update === 'function' ? update(get(baseAtom)) : update;
+      set(baseAtom, nextValue);
+      localStorage.setItem(key, JSON.stringify(nextValue));
+    },
+  );
+  return derivedAtom;
+}
+
+export const usernameAtom = atomWithLocalStorage<string>('username', '');
